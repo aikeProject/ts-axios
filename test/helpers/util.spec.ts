ae21ddcd341ce7821908f44d376b7a46e9eaa1c8
成雨 <rensailong@zhongan.io>
2019-07-17 10:24:42 +0800

    util模块测试

--- test/helpers/util.spec.ts
+++ test/helpers/util.spec.ts
@@ -51,4 +51,87 @@ describe('helpers:util', () => {
       expect(isURLSearchParams('foo=1&bar=2')).toBeFalsy()
     })
   })
+
+  describe('extend', function() {
+    test('should be mutable', () => {
+      const a = Object.create(null)
+      const b = { foo: 123 }
+      extend(a, b)
+
+      expect(a.foo).toBe(123)
+    })
+
+    test('should extend properties 合并所有属性', () => {
+      const a = { foo: 123, bar: 456 }
+      const b = { bar: 789 }
+      const c = extend(a, b)
+
+      expect(c.foo).toBe(123)
+      expect(c.bar).toBe(789)
+    })
+  })
+
+  describe('deepMerge', () => {
+    test('should be immutable（不可改变）', () => {
+      const a = Object.create(null)
+      const b: any = { foo: 123 }
+      const c: any = { bar: 456 }
+
+      deepMerge(a, b, c)
+
+      expect(typeof a.foo).toBe('undefined')
+      expect(typeof a.bar).toBe('undefined')
+      expect(typeof b.bar).toBe('undefined')
+      expect(typeof c.foo).toBe('undefined')
+    })
+
+    test('should deepMerge properties 合并所有属性', () => {
+      const a = { foo: 123 }
+      const b = { bar: 456 }
+      const c = { foo: 789 }
+      const d = deepMerge(a, b, c)
+
+      expect(d.foo).toBe(789)
+      expect(d.bar).toBe(456)
+    })
+
+    test('should deepMerge recursively 递归合并', () => {
+      const a = { foo: { bar: 123 } }
+      const b = { foo: { baz: 456 }, bar: { qux: 789 } }
+      const c = deepMerge(a, b)
+
+      // toEqual 断言引用类型 比较的是对象结构是否相同
+      expect(c).toEqual({
+        foo: {
+          bar: 123,
+          baz: 456
+        },
+        bar: {
+          qux: 789
+        }
+      })
+    })
+
+    test('should remove all references form nested object', () => {
+      const a = { foo: { bar: 123 } }
+      const b = {}
+      const c = deepMerge(a, b)
+
+      expect(c).toEqual({
+        foo: {
+          bar: 123
+        }
+      })
+    })
+
+    test('should handle null and undefined arguments', () => {
+      expect(deepMerge(undefined, undefined)).toEqual({})
+      expect(deepMerge(undefined, { foo: 123 })).toEqual({ foo: 123 })
+      expect(deepMerge({ foo: 123 }, undefined)).toEqual({ foo: 123 })
+
+      expect(deepMerge(null, null)).toEqual({})
+      expect(deepMerge(null, { foo: 123 })).toEqual({ foo: 123 })
+      expect(deepMerge({ foo: 123 }, null)).toEqual({ foo: 123 })
+    })
+  })
 })
